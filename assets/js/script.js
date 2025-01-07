bb9790c05f359135304657a465fe50610f19b021
Emmanuel <emmanuelberberi@hotmail.com>
2025-01-06 21:03:09 -0500

    JS navbar toggle added

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// add event on element :
+//This function is a utility function designed to add an event listener 
+//to one or more DOM elements, improving reusability and reducing repetitive code
+const addEventOnElem = function (elem, type, callback) {
+    if(elem.length > 1) {
+        for (let i = 0 ; i < elem.length; i++) {
+            elem[i].addEventListener(type, callback);
+        } 
+    } else {
+        elem.addEventListener(type, callback);
+    }  
+}
+
+//navbar toggle
+
+const navbar = document.querySelector("[data-navbar]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
+
+const toggleNavBar = () => navbar.classList.toggle("active");
+
+addEventOnElem(navToggler, "click", toggleNavBar);
